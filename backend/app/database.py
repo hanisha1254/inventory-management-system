fbea256d9cb5c4ea08d43d9fa2c10cdd86259053
Hanisha Gautam <haneeshaaa@gmail.com>
2026-06-15 17:26:49 +0530

    Fix database config

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,10 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:postgres@localhost:5432/inventory_db"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
